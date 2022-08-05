05e22d7f43b1b38fcb347d739cc4871023ac0b7d
Szymon <regicsmaster@gmail.com>
2022-08-05 09:50:54 +0200

    Added First Controller

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using MVCCourseApp.Models;
+
+namespace MVCCourseApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "CAT IN BOOTS" };
+            return View(movie);
+        }
+    }
+}
