d0d208b9c0c95c1e0327c71fe594290708dd9307
Szymon <regicsmaster@gmail.com>
2022-08-05 10:21:50 +0200

    Added custom actions

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -10,5 +10,19 @@ namespace MVCCourseApp.Controllers
             var movie = new Movie() { Name = "CAT IN BOOTS" };
             return View(movie);
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        }
     }
 }
